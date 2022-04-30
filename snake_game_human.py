530c5e7d4cf26b3f9aa000224ed2312a0c329099
Brandon O'Briant <obrianbl@protonmail.com>
2022-04-30 09:45:02 -0800

    got game screen to deploy

--- snake_game_human.py
+++ snake_game_human.py
@@ -7,3 +7,14 @@ from collections import namedtuple
 #https://www.pygame.org/docs/ref/pygame.html#pygame.init
 pygame.init()
 font = pygame.font.Font('arial.tff', 25)
+
+class SnakeGame:
+    def __init__(self, w = 640, h = 480):
+        """
+        
+        Args:
+            w (int): width of game screen
+            h (int): height of game screen
+        Returns:
+            None
+        """

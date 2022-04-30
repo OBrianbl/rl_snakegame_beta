b391fcbfd83c55f23e93869d088055733efb0431
Brandon O'Briant <obrianbl@protonmail.com>
2022-04-30 09:04:25 -0800

    something about what you did

---
+++ snake_game_human.py
@@ -0,0 +1,9 @@
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+# search pygame pygame.init()
+#https://www.pygame.org/docs/ref/pygame.html#pygame.init
+pygame.init()
+font = pygame.font.Font('arial.tff', 25)
